935b5dc405595d7116795c074de836f83a128f2a
Usman <149417807+UsmanA33@users.noreply.github.com>
2025-11-12 16:25:14 +0100

    Delete Day2-04.js

--- Day2-04.js
+++
@@ -1,8 +0,0 @@
-let a = 10;
-let b = 3;
-
-console.log("Addition:", a + b);
-console.log("Subtraction:", a - b);
-console.log("Multiplication:", a * b);
-console.log("Division:", a / b);
-console.log("Remainder:", a % b);
