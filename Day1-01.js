52787acf864c558537bb997d08b140496a8f38d6
Usman <149417807+UsmanA33@users.noreply.github.com>
2025-11-12 16:19:03 +0100

    Add files via upload

---
+++ Day1-01.js
@@ -0,0 +1,5 @@
+console.log("My name is John");
+console.log("I am 28 years old");
+console.log("My favorite color is green");
+console.log("I'm learning coding");
+console.log("JavaScript is interesting!");
---
+++ Day6-12.js
@@ -0,0 +1,6 @@
+function addNumbers(a, b) {
+  return a + b;
+}
+
+let result = addNumbers(10, 7);
+console.log("The sum is:", result);
