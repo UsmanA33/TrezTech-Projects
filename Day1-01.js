ada27389f7b9709bc98a7032a7c4681f9b450973
Usman <149417807+UsmanA33@users.noreply.github.com>
2025-11-12 16:24:24 +0100

    Delete Day1-01.js

--- Day1-01.js
+++
@@ -1,5 +0,0 @@
-console.log("My name is John");
-console.log("I am 28 years old");
-console.log("My favorite color is green");
-console.log("I'm learning coding");
-console.log("JavaScript is interesting!");
