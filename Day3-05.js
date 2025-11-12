a54e8e1525a75761bfeb0d2e0bda95ee299328c3
Usman <149417807+UsmanA33@users.noreply.github.com>
2025-11-12 16:25:27 +0100

    Delete Day3-05.js

--- Day3-05.js
+++
@@ -1,9 +0,0 @@
-let number = -5;
-
-if (number > 0) {
-  console.log("The number is positive");
-} else if (number < 0) {
-  console.log("The number is negative");
-} else {
-  console.log("The number is zero");
-}
